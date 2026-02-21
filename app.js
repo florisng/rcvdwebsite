298c9ac2413aa7ebe1a65b4cdaf33bce03517047
Floris Ng <florisngendahayo@gmail.com>
2026-02-21 12:32:19 +0100

    Add the link for resgitration 10

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-// Serve static files from "partials" folder
-app.use('/partials', express.static(path.join(__dirname, 'views/partials')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
